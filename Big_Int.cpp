a073445272f0733771a663ef30b7cd95cb1e0ffa
Tsybulka Mikita <tsybulka.mikita.pl@gmail.com>
2023-07-31 15:27:27 +0200

    Updating Big_Int functions Adding unit tests

--- Big_Int.cpp
+++ Big_Int.cpp
@@ -3,16 +3,356 @@
 #include "Big_Int.h"
 using namespace std;
 
-int main() {
-    string a_s, b_s;
-    unsigned long long b;
-    cin >> a_s >> b_s >> b;
+int unit_test_for_long_max() {
+    {
+        if (long_max("1", "3") != "3") {
+            cout << "3 > 1" << endl;
+            return -1;
+        }
+    }
+    {
+        if (long_max("11", "3") != "11") {
+            cout << "11 > 3" << endl;
+            return -2;
+        }
+    }
+    {
+        if (long_max("125", "9") != "125") {
+            cout << "125 > 9" << endl;
+            return -3;
+        }
+    }
+    {
+        if (long_max("-125", "9") != "9") {
+            cout << "9 > -125" << endl;
+            return -4;
+        }
+    }
+    {
+        if (long_max("-125", "-15") != "-15") {
+            cout << "-15 > -125" << endl;
+            return -5;
+        }
+    }
+    {
+        //Empty Big_int control
+        //long_max("", "-15");
+    }
+    {
+        if (long_max("0", "-15") != "0") {
+            cout << "0 > -15" << endl;
+            return -6;
+        }
+    }
 
-    cout << long_max(a_s, b_s) << '\n'; // max num from a and b
-    cout << long_min(a_s, b_s) << '\n'; // min num from a and b
-    cout << long_sum(a_s, b_s) << '\n'; //sum of a and b
-    cout << long_diff(a_s, b_s) << '\n'; // difference from a to b
-    cout << long_multi(a_s, b_s) << '\n'; // multiplication  of a and b
-    cout << long_pow(a_s, b) << '\n'; // power a to b
-    cout << long_div(a_s, b_s) << '\n'; // division a to b
+    cout << "OK";
+    return 0;
+}
+
+int unit_test_for_long_min() {
+    {
+        if (long_min("1", "3") != "1") {
+            cout << "3 > 1" << endl;
+            return -1;
+        }
+    }
+    {
+        if (long_min("11", "3") != "3") {
+            cout << "11 > 3" << endl;
+            return -2;
+        }
+    }
+    {
+        if (long_min("125", "9") != "9") {
+            cout << "125 > 9" << endl;
+            return -3;
+        }
+    }
+    {
+        if (long_min("-125", "9") != "-125") {
+            cout << "9 > -125" << endl;
+            return -4;
+        }
+    }
+    {
+        if (long_min("-125", "-9") != "-125") {
+            cout << "-9 > -125" << endl;
+            return -5;
+        }
+    }
+    {
+        //Empty Big_int control
+        //long_min("", "-15");
+    }
+    {
+        if (long_min("125", "-9") != "-9") {
+            cout << "125 > -9" << endl;
+            return -6;
+        }
+    }
+
+    cout << "OK";
+    return 0;
+}
+
+int unit_test_for_long_sum() {
+    {
+        if (long_sum("1", "3") != "4") {
+            cout << "1 + 3 != " << long_sum("1", "3") << endl;
+            return -1;
+        }
+    }
+    {
+        if (long_sum("11", "3") != "14") {
+            cout << "11 + 3 != " << long_sum("11", "3") << endl;
+            return -2;
+        }
+    }
+    {
+        if (long_sum("125", "9") != "134") {
+            cout << "125 + 9 != " << long_sum("125", "9") << endl;
+            return -3;
+        }
+    }
+    {
+        if (long_sum("-125", "9") != "-116") {
+            cout << "-125 + 9 != " << long_sum("-125", "9") << endl;
+            return -4;
+        }
+    }
+    {
+        if (long_sum("125", "-9") != "116") {
+            cout << "125 + (-9) != " << long_sum("125", "-9");
+            return -5;
+        }
+    }
+    {
+        //Empty Big_int control
+        //long_sum("", "-15");
+    }
+    {
+        if (long_sum("-125", "-9") != "-134") {
+            cout << "-125 + (-9) != " << long_sum("-125", "-9");
+            return -6;
+        }
+    }
+    {
+        if (long_sum("0", "0") != "0") {
+            cout << "0 + 0 != " << long_sum("0", "0");
+            return -7;
+        }
+    }
+
+
+    cout << "OK";
+    return 0;
+}
+
+int unit_test_for_long_dif_positive() {
+    {
+        if (long_dif_positive("3", "1") != "2") {
+            cout << "3 - 1 != " << long_dif_positive("3", "1") << endl;
+            return -1;
+        }
+    }
+    {
+        if (long_dif_positive("11", "3") != "8") {
+            cout << "11 + 3 != " << long_dif_positive("11", "3") << endl;
+            return -2;
+        }
+    }
+    {
+        if (long_dif_positive("125", "9") != "116") {
+            cout << "125 - 9 != " << long_dif_positive("125", "9") << endl;
+            return -3;
+        }
+    }
+    {
+        if (long_dif_positive("1000", "1") != "999") {
+            cout << "1000 - 1 != " << long_dif_positive("1000", "1") << endl;
+            return -4;
+        }
+    }
+    {
+        if (long_dif_positive("100", "125") != "-25") {
+            cout << "100 - 125 != " << long_dif_positive("125", "-9");
+            return -5;
+        }
+    }
+    {
+        //Empty Big_int control
+        long_dif_positive("", "-15");
+    }
+    {
+        if (long_dif_positive("19", "32") != "-13") {
+            cout << "19 - 32 != " << long_dif_positive("19", "32");
+            return -6;
+        }
+    }
+
+    cout << "OK";
+    return 0;
+}
+
+int unit_test_for_long_dif() {
+    {
+        if (long_dif("-125", "-15") != "-110") {
+            cout << "-125 - (-15) != " << long_dif("-125", "-15") << endl;
+            return -1;
+        }
+    }
+    {
+        if (long_dif("-125", "15") != "-140") {
+            cout << "-125 - 15 != " << long_dif("-125", "15") << endl;
+            return -2;
+        }
+    }
+    {
+        if (long_dif("125", "-15") != "140") {
+            cout << "125 - 9 != " << long_dif("125", "-15") << endl;
+            return -3;
+        }
+    }
+    {
+        if (long_dif("125", "15") != "110") {
+            cout << "125 - 15 != " << long_dif("125", "15") << endl;
+            return -4;
+        }
+    }
+    {
+        //Empty Big_int control
+        //long_dif("", "-15");
+    }
+    
+
+    cout << "OK";
+    return 0;
+}
+
+int unit_test_for_long_multi() {
+    {
+        if (long_multi("125", "5") != "625") {
+            cout << "125 * 5 != " << long_multi("125", "5") << endl;
+            return -1;
+        }
+    }
+    {
+        if (long_multi("125", "10") != "1250") {
+            cout << "125 * 10 != " << long_multi("125", "10") << endl;
+            return -2;
+        }
+    }
+    {
+        if (long_multi("125", "-5") != "-625") {
+            cout << "125 * (-5) != " << long_multi("125", "-5") << endl;
+            return -3;
+        }
+    }
+    {
+        if (long_multi("-125", "-5") != "625") {
+            cout << "-125 * (-5) != " << long_multi("-125", "-15") << endl;
+            return -4;
+        }
+    }
+    {
+        //Empty Big_int control
+        //long_multi("", "-15");
+    }
+
+    cout << "OK";
+    return 0;
+}
+
+int unit_test_for_long_pow() {
+    {
+        if (long_pow("2", 3) != "8") {
+            cout << "2 ^ 3 != " << long_pow("2", 3) << endl;
+            return -1;
+        }
+    }
+    {
+        //Empty Big_int control
+        //long_pow("", 1);
+    }
+
+    cout << "OK";
+    return 0;
+}
+
+int unit_test_for_long_div() {
+    {
+        if (long_div("10", "2") != "5") {
+            cout << "10 / 2 != " << long_div("10", "2") << endl;
+            return -1;
+        }
+    }
+    {
+        if (long_div("22", "4") != "5") {
+            cout << "22 / 4 != " << long_div("22", "4") << endl;
+            return -2;
+        }
+    }
+     {
+        if (long_div("18446744073709551618", "2") != "9223372036854775809") {
+            cout << "18446744073709551618 / 2 != " << long_div("18446744073709551618", "2") << endl;
+            return -3;
+        }
+    }
+    {
+        if (long_div("0", "2") != "0") {
+            cout << "0 / 2 != " << long_div("0", "2") << endl;
+            return -4;
+        }
+    }
+    {
+        if (long_div("18446744073709551618", "0") != "") {
+            cout << "18446744073709551618 / 0 != " << long_div("18446744073709551618", "0") << endl;
+            return -5;
+        }
+    }
+    {
+        if (long_div("5", "1") != "5") {
+            cout << "5 / 1 != " << long_div("5", "1") << endl;
+            return -6;
+        }
+    }
+    {
+        //Empty Big_int control
+        long_div("", "1");
+    }
+
+    cout << "OK";
+    return 0;
+}
+
+int unit_test_for_long_remainder_of_div() {
+    {
+        if (long_remainder_of_div("11", "2") != "1") {
+            cout << "11 % 2 != " << long_remainder_of_div("11", "2") << endl;
+            return -1;
+        }
+    }
+    {
+        if (long_remainder_of_div("22", "4") != "2") {
+            cout << "22 % 4 != " << long_remainder_of_div("22", "4") << endl;
+            return -2;
+        }
+    }
+    {
+        if (long_remainder_of_div("2", "1") != "0") {
+            cout << "2 % 1 != " << long_remainder_of_div("2", "1") << endl;
+            return -3;
+        }
+    }
+    {
+        //Empty Big_int control
+        long_remainder_of_div("", "1");
+    }
+
+    cout << "OK";
+    return 0;
+}
+
+int main() {
+    return 0; // Possible to put some utit tests here
 }
--- Big_Int.h
+++ Big_Int.h
@@ -1,202 +1,304 @@
 #pragma once
+#include <string>
+#include <algorithm>
 using namespace std;
 
-string long_max(string a_s, string b_s) {
+void equle_to_legnth(string& a_s, string& b_s) {
     if (a_s.length() > b_s.length()) {
         string doner(a_s.length() - b_s.length(), '0');
         b_s = doner + b_s;
     }
-    else if (b_s.length() > a_s.length()) {
+    else if (a_s.length() < b_s.length()) {
         string doner(b_s.length() - a_s.length(), '0');
         a_s = doner + a_s;
     }
-
-    if (a_s > b_s) return a_s;
-    else return b_s;
 }
 
-string long_min(string a_s, string b_s) {
-    if (a_s.length() > b_s.length()) {
-        string doner(a_s.length() - b_s.length(), '0');
-        b_s = doner + b_s;
-    }
-    else if (b_s.length() > a_s.length()) {
-        string doner(b_s.length() - a_s.length(), '0');
-        a_s = doner + a_s;
+string long_max(string a_s, string b_s);
+string long_min(string a_s, string b_s);
+string long_sum(string a_s, string b_s);
+string long_dif_positive(string a_s, string b_s);
+string long_dif(string a_s, string b_s);
+string long_multi(string a, string b);
+string long_pow(string x, unsigned long long p);
+string long_remainder_of_div(string a_s, string b_s);
+
+//////////////////////////////////////////////////
+
+string long_max(const string a,const string b) {
+    if (a.empty() || b.empty()) {
+        cout << "One of the element in long_max() is empty!" << endl;
+        return "";
     }
 
-    if (a_s < b_s) return a_s;
-    else return a_s;
+    string a_s = a, b_s = b;
+
+    if (a_s[0] == '-' && b_s[0] == '-') {
+        a_s.erase(0,1);
+        b_s.erase(0,1);
+        return "-" + long_min(a_s, b_s);
+    }
+    
+    equle_to_legnth(a_s, b_s);
+    if (a_s > b_s) return a;
+    else return b;
 }
 
-string long_sum(string a_s, string b_s) {
-    if (a_s.length() > b_s.length()) {
-        string doner(a_s.length() - b_s.length(), '0');
-        b_s = doner + b_s;
+string long_min(const string a,const string b) {
+    if (a.empty() || b.empty()) {
+        cout << "One of the element in long_min() is empty!" << endl;
+        return "";
     }
-    else if (b_s.length() > a_s.length()) {
-        string doner(b_s.length() - a_s.length(), '0');
-        a_s = doner + a_s;
+
+    string a_s = a, b_s = b;
+
+    if (a_s[0] == '-' && b_s[0] == '-') {
+        a_s.erase(0, 1);
+        b_s.erase(0, 1);
+        return "-" + long_max(a_s, b_s);
+    }
+    
+    equle_to_legnth(a_s, b_s);
+    if (a_s < b_s) return a;
+    else return b;
+    
+}
+
+string long_sum(string a, string b) {
+    if (a.empty() || b.empty()) {
+        cout << "One of the element in long_sum() is empty!" << endl;
+        return "";
     }
 
     int in_mind = 0;
-    string rez;
-    int a, b;
+    string result;
+    int a_digit, b_digit;
+    string sign;
 
-    for (int i = a_s.length() - 1; i >= 0; i--) {
-        a = (a_s[i] - '0') + in_mind;
-        b = b_s[i] - '0';
-        in_mind = (a + b) / 10;
-        rez += to_string((a + b) % 10);
+    if (a[0] == '-' && b[0] == '-') {
+        a.erase(0, 1);
+        b.erase(0, 1);
+        sign = "-";
+    }
+    else if (a[0] == '-') {
+        a.erase(0, 1);
+        return long_dif(b, a);
+    }
+    else if (b[0] == '-') {
+        b.erase(0, 1);
+        return long_dif(a, b);
     }
 
-    if (in_mind != 0) rez.push_back('1');
-    reverse(rez.begin(), rez.end());
-    return rez;
+    equle_to_legnth(a, b);
+
+    for (int i = a.length() - 1; i >= 0; i--) {
+        a_digit = (a[i] - '0') + in_mind;
+        b_digit = b[i] - '0';
+        in_mind = (a_digit + b_digit) / 10;
+        result += to_string((a_digit + b_digit) % 10);
+    }
+
+    if (in_mind != 0) result.push_back('1');
+    result += sign;
+    reverse(result.begin(), result.end());
+
+    return result;
 }
 
-string long_diff(string a_s, string b_s) {
-    if (a_s.length() > b_s.length()) {
-        string doner(a_s.length() - b_s.length(), '0');
-        b_s = doner + b_s;
-    }
-    else if (b_s.length() > a_s.length()) {
-        string doner(b_s.length() - a_s.length(), '0');
-        a_s = doner + a_s;
+string long_dif_positive(string a_s, string b_s) {
+    if (a_s.empty() || b_s.empty()) {
+        cout << "One of the element in long_dif_positive() is empty!" << endl;
+        return "";
     }
 
-    char zn = '+';
-    if (a_s < b_s) {
+    int in_mind = 0;
+    string result;
+    int a_digit, b_digit;
+    string sign;
+
+    equle_to_legnth(a_s, b_s);
+
+    if (long_max(a_s, b_s) == b_s) {
         swap(a_s, b_s);
-        zn = '-';
+        sign = "-";
     }
-
-    int in_mind = 0;
-    string rez;
-    int a, b;
 
     for (int i = a_s.length() - 1; i >= 0; i--) {
-        a = (a_s[i] - '0') + in_mind;
-        b = b_s[i] - '0';
-        if (a - b < 0) {
+        a_digit = (a_s[i] - '0') + in_mind;
+        b_digit = b_s[i] - '0';
+        in_mind = 0;
+        if (a_digit - b_digit < 0) {
             in_mind = -1;
-            a += 10;
+            a_digit += 10;
         }
-        rez += to_string(a - b);
+        result += to_string(a_digit - b_digit);
     }
 
-    while (rez[rez.length() - 1] == '0') rez.erase(a_s.length() - 1, 1);
-    rez.push_back(zn);
-    reverse(rez.begin(), rez.end());
-    return rez;
+    while (result[result.length() - 1] == '0') result.erase(a_s.length() - 1, 1);
+    result += sign;
+    reverse(result.begin(), result.end());
+    return result;
 }
 
-string long_multi(string a_s, string b_s) {
-    if (a_s == "0" || b_s == "0") return "0";
-
-    string b_s_doner, a_s_doner;
-    if (a_s.length() > b_s.length()) {
-        string doner(a_s.length() - b_s.length(), '0');
-        b_s_doner = doner + b_s;
-    }
-    else if (b_s.length() > a_s.length()) {
-        string doner(b_s.length() - a_s.length(), '0');
-        a_s_doner = doner + a_s;
+string long_dif(string a, string b) {
+    if (a.empty() || b.empty()) {
+        cout << "One of the element in long_dif() is empty!" << endl;
+        return "";
     }
 
-    if (a_s_doner > b_s_doner) swap(a_s, b_s);
+    if (a[0] == '-' && b[0] == '-') {
+        a.erase(0, 1);
+        b.erase(0, 1);
+        swap(a, b);
+    }
+    else if (a[0] == '-') {
+        a.erase(0, 1);
+        return "-" + long_sum(a, b);
+    }
+    else if (b[0] == '-') {
+        b.erase(0, 1);
+        return long_sum(a, b);
+    }
+    
+    int in_mind = 0;
+    string result;
+    int a_digit, b_digit;
+    string sign;
+
+    equle_to_legnth(a, b);
+
+    if (long_max(a, b) == b && a != b) {
+        swap(a, b);
+        sign = "-";
+    }
+
+    for (int i = a.length() - 1; i >= 0; i--) {
+        a_digit = (a[i] - '0') + in_mind;
+        b_digit = b[i] - '0';
+        in_mind = 0;
+        if (a_digit - b_digit < 0) {
+            in_mind = -1;
+            a_digit += 10;
+        }
+        result += to_string(a_digit - b_digit);
+    }
+
+    while (result[result.length() - 1] == '0' && result != "0") result.erase(a.length() - 1, 1);
+    result += sign;
+    reverse(result.begin(), result.end());
+    return result;
+    
+}
+
+string long_multi(string a, string b) {
+    if (a.empty() || b.empty()) {
+        cout << "One of the element in long_multi() is empty!" << endl;
+        return "";
+    }
+
+    //if (a == "0" || b == "0") return "0";
+
+    if (long_max(a, b) == a) swap(a, b);
 
     int in_mind = 0;
-    string rez = "0", rez_f;
-    int rez_i;
-    int a, b;
+    string result = "0", step_result;
+    string sign = "";
+    int in_mind_i;
+    int a_digit, b_digit;
 
-    for (int e = b_s.length() - 1; e >= 0; e--) {
-        for (int i = a_s.length() - 1; i >= 0; i--) {
-            a = a_s[i] - '0';
-            b = b_s[e] - '0';
+    if (a[0] == '-' && b[0] == '-') {
+        a.erase(0, 1);
+        b.erase(0, 1);
+    }
+    else if (a[0] == '-') {
+        a.erase(0, 1);
+        sign = "-";
+    }
+    else if (b[0] == '-') {
+        b.erase(0, 1);
+        sign = "-";
+    }
 
-            rez_i = (a * b) + in_mind;
+    for (int e = b.length() - 1; e >= 0; e--) {
+        for (int i = a.length() - 1; i >= 0; i--) {
+            a_digit = a[i] - '0';
+            b_digit = b[e] - '0';
 
-            in_mind = rez_i / 10;
+            in_mind_i = (a_digit * b_digit) + in_mind;
 
-            rez_f += to_string(rez_i % 10);
+            in_mind = in_mind_i / 10;
+
+            step_result += to_string(in_mind_i % 10);
         }
-        if (in_mind != 0) rez_f.push_back(char(in_mind + '0'));
+        if (in_mind != 0) step_result.push_back(char(in_mind + '0'));
 
-        reverse(rez_f.begin(), rez_f.end());
+        reverse(step_result.begin(), step_result.end());
 
-        string doner(b_s.length() - 1 - e, '0');
-        rez_f += doner;
+        string doner(b.length() - 1 - e, '0');
+        step_result += doner;
 
-        rez = long_sum(rez, rez_f);
+        result = long_sum(result, step_result);
 
-        rez_f = "";
+        step_result = "";
         doner = "";
         in_mind = 0;
     }
-    return rez;
+    result = sign + result;
+    return result;
 }
 
 string long_pow(string x, unsigned long long p) {
-    string rez = "1";
-    for (unsigned long long i = 0; i < p; i++) rez = long_multi(rez, x);
-    return rez;
-}
-
-string long_num_div(string a_s, unsigned long long b) {
-    if (a_s == "0") return "0";
-    unsigned long long def = 0;
-    string rez;
-
-    int cnt = 1;
-    while (stoi(a_s.substr(0, cnt)) / b == 0) cnt++;
-    unsigned long long a_p = stoi(a_s.substr(0, cnt));
-
-    for (unsigned long long i = cnt; i <= a_s.size(); i++) {
-        rez += to_string(a_p / b);
-        def = a_p % b;
-        a_p = a_s[i] - '0';
-        if (def != 0) a_p += 10 * def;
+    if (x.empty()) {
+        cout << "Element in long_pow() is empty!" << endl;
+        return "";
     }
+    else if (p < 0) return "0";
 
-    return rez;
+    string result = "1";
+    for (unsigned long long i = 0; i < p; i++) result = long_multi(result, x);
+    return result;
 }
-
-string long_s_to_s_div(string a_s, string b_s) {
-    string rez = "0";
-    while (long_max(long_multi(long_sum(rez, "1"), b_s), a_s) == a_s) {
-        rez = long_sum(rez, "1");
-    }
-    return rez;
-} // so slow
 
 string long_div(string a_s, string b_s) {
+    if (b_s == "0" || b_s.empty() || a_s.empty()) {
+        cout << "Element in long_pow() is empty or division to 0!" << endl;
+        return "";
+    }
+    else if (a_s == "0") return "0";
+
     string num_max = "18446744073709551615";
-    string rez;
+    string result;
+
     if (long_max(num_max,b_s) == num_max) {
-        unsigned long long b = stoi(b_s);
+        unsigned long long b = stol(b_s);
         unsigned long long def = 0;
 
         int cnt = 1;
-        while (stoi(a_s.substr(0, cnt)) / b == 0) cnt++;
+        while (stol(a_s.substr(0, cnt)) / b == 0) cnt++;
         unsigned long long a_p = stoi(a_s.substr(0, cnt));
 
         for (unsigned long long i = cnt; i <= a_s.size(); i++) {
-            rez += to_string(a_p / b);
+            result += to_string(a_p / b);
             def = a_p % b;
-            a_p = a_s[i] - '0';
+            a_p = int(a_s[i] - '0');
             if (def != 0) a_p += 10 * def;
         }
-
-        while (rez[0] == '0') rez.erase(0, 1);
     }
     else {
-        rez = "0";
-        while (long_max(long_multi(long_sum(rez, "1"), b_s), a_s) == a_s) {
-            rez = long_sum(rez, "1");
+        result = "0";
+        while (long_max(long_multi(long_sum(result, "1"), b_s), a_s) == a_s)
+        {
+            result = long_sum(result, "1");
         }
     }
 
-    return rez;
+    return result;
+}
+
+string long_remainder_of_div(string a_s, string b_s) { //Remainder of the division
+    if (a_s.empty() || b_s.empty()) {
+        cout << "One of the element in long_remainder_of_div() is empty!" << endl;
+        return "";
+    }
+    return long_dif(a_s, long_multi(long_div(a_s, b_s), b_s));
 }
